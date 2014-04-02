adab4033b81f0f6e15fe5ea8bb31b4de21435bee
Gonzo345 <gonzo345@gmail.com>
2014-04-02 13:20:51 +0200

    Sine Dolore funcionando
    Funcionando con WebViews

--- AppSineDolore/src/apps/sine/appsinedolore/PDF.java
+++ AppSineDolore/src/apps/sine/appsinedolore/PDF.java
@@ -2,14 +2,35 @@ package apps.sine.appsinedolore;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class PDF  extends Activity {
+	
+	private WebView mWebView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.pdf);
+		//setContentView(R.layout.pdf);
 
+//		WebView vista = (WebView)findViewById(R.id.page);
+//		vista.loadUrl("http://www.painmeeting.org/pdfs/programa2014.pdf");
+		
+		mWebView = new WebView(this);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		final Activity activity = this;
+		
+		mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mWebView.loadUrl("https://docs.google.com/gview?embedded=true&url="+"http://www.painmeeting.org/pdfs/programa2014.pdf");
+        setContentView(mWebView );
+		
 	}
 	
 }
--- AppSineDolore/src/apps/sine/appsinedolore/Wine.java
+++ AppSineDolore/src/apps/sine/appsinedolore/Wine.java
@@ -2,13 +2,30 @@ package apps.sine.appsinedolore;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Wine  extends Activity {
 
+	private WebView mWebView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.wine);
+		//setContentView(R.layout.wine);
+		
+		mWebView = new WebView(this);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		final Activity activity = this;
+		
+		mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mWebView.loadUrl("shop.sinedolore.org");
+        setContentView(mWebView );
 
 	}
 	
