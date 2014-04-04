d5239606cfda08d3b8252ed028b2520c8ff2c9cb
AitorCJ <aitorcosta@gmail.com>
2014-04-04 09:27:31 +0200

    Mezclado!

--- AppSineDolore/src/apps/sine/appsinedolore/Youtube.java
+++ AppSineDolore/src/apps/sine/appsinedolore/Youtube.java
@@ -1,6 +1,7 @@
 package apps.sine.appsinedolore;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 public class Youtube extends Activity {
 
 	private WebView mWebView;
+	private MediaPlayer mediaplayer;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,19 +20,37 @@ public class Youtube extends Activity {
 		mWebView = new WebView(this);
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		final Activity activity = this;
-		
+
 		mWebView.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
-            }
-        });
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				Toast.makeText(activity, description, Toast.LENGTH_SHORT)
+						.show();
+			}
+		});
 
-        mWebView.loadUrl("https://m.youtube.com/user/SineDolore/feed");
-        setContentView(mWebView );
+		setContentView(mWebView);
 
-		// Tenemos que hacer que la visita a YouTube sea por medio de la API
-		
-		
+		if (savedInstanceState != null) {
+			mWebView.restoreState(savedInstanceState);
+		} else {
+			mWebView.loadUrl("https://m.youtube.com/user/SineDolore/feed");
+		}
+
+		// Tenemos que pillar el seekto
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle guardar) {
+		super.onSaveInstanceState(guardar);
+
+		mWebView.saveState(guardar);
+
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
 
 	}
 
