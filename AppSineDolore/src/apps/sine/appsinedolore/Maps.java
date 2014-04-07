ffd958f0dd965bc1ba4744f1c3f3d7778c7e44d3
Gonzo345 <gonzo345@gmail.com>
2014-04-07 11:19:42 +0200

    GMaps
    Google Maps implementado y funcionando. Faltan puntos en mapa
    (futurible).

--- AppSineDolore/src/apps/sine/appsinedolore/Maps.java
+++ AppSineDolore/src/apps/sine/appsinedolore/Maps.java
@@ -3,12 +3,13 @@ package apps.sine.appsinedolore;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class Maps extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.maps);	//Falta layout maps (Google Maps)
+		setContentView(R.layout.maps);
 		
 	}
 }
