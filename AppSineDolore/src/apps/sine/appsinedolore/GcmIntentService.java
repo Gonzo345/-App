51f2946f440f9c9f7c771e1c1167854327d6ed0d
AitorCJ <aitorcosta@gmail.com>
2014-05-02 20:34:37 +0200

    registro en POST con Sender ID

--- AppSineDolore/src/apps/sine/appsinedolore/GcmIntentService.java
+++ AppSineDolore/src/apps/sine/appsinedolore/GcmIntentService.java
@@ -1,156 +1,59 @@
 package apps.sine.appsinedolore;
 
 import android.app.IntentService;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gcm.GCMBaseIntentService;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
-public class GcmIntentService extends GCMBaseIntentService{
+public class GCMIntentService extends IntentService 
+{
+	private static final int NOTIF_ALERTA_ID = 1;
 
+	public GCMIntentService() {
+        super("GCMIntentService");
+    }
+	
 	@Override
-	protected void onError(Context arg0, String arg1) {
-		// TODO Auto-generated method stub
-		Toast.makeText(this, "error", Toast.LENGTH_LONG).show();
-	}
+    protected void onHandleIntent(Intent intent) 
+	{
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        
+        String messageType = gcm.getMessageType(intent);
+        Bundle extras = intent.getExtras();
 
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-		String msg = intent.getExtras().getString("msg");
+        if (!extras.isEmpty()) 
+        {  
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) 
+            {
+            	mostrarNotification(extras.getString("msg"));
+            }
+        }
+        
+        GCMBroadcastReceiver.completeWakefulIntent(intent);
+    }
+	
+	private void mostrarNotification(String msg) 
+	{
+		NotificationManager mNotificationManager =    
+				(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); 
 		
-		mostrarNotificaciones(context, msg);
-	}
-
-	private void mostrarNotificaciones(Context context, String msg) {
-		// TODO Auto-generated method stub
-		String ms = Context.NOTIFICATION_SERVICE;
-		NotificationManager notificationManager = (NotificationManager)context.getSystemService(ms);
+		NotificationCompat.Builder mBuilder = 
+			new NotificationCompat.Builder(this)  
+				.setSmallIcon(android.R.drawable.stat_sys_warning)  
+				.setContentTitle("Notificaci√≥n GCM")  
+				.setContentText(msg);
 		
-		//Configuramos la notificación
-		int icono = android.R.drawable.star_on;
-		CharSequence textoEstado="Buenas notificación Push!";
-		long hora = System.currentTimeMillis();
+		Intent notIntent =  new Intent(this, Inicial.class);    
+		PendingIntent contIntent = PendingIntent.getActivity(     
+				this, 0, notIntent, 0);   
 		
-		Notification notificacion = new Notification(icono, textoEstado, hora);
+		mBuilder.setContentIntent(contIntent);
 		
-		//Confihuramos el intent 
-		Context contexto =context.getApplicationContext();
-		CharSequence titulo="Nuevo mensaje";
-		CharSequence descripcion= msg;
-		
-		Intent notintent = new Intent(contexto, GcmIntentService.class);
-		
-		PendingIntent contIntent = PendingIntent.getActivity(contexto, 0, notintent, 0);
-		
-		//Al pulsar en la notificacion esta desaparecerá
-		notificacion.flags |= Notification.FLAG_AUTO_CANCEL;
-		
-	 //Enviar Notificacion
-		notificationManager.notify(1, notificacion);
-	}
-
-	@Override
-	protected void onRegistered(Context arg0, String regId) {
-		// TODO Auto-generated method stub
-		registroServidor(regId);
-	}
-
-	private void registroServidor(String regId) {
-		// Encargado de la conexión con el servidor
-		
-	}
-
-	@Override
-	protected void onUnregistered(Context arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
+		mNotificationManager.notify(NOTIF_ALERTA_ID, mBuilder.build());
+    }
 }
-//	public static final int NOTIFICATION_ID = 1;
-//    private NotificationManager mNotificationManager;
-//    NotificationCompat.Builder builder;
-//    
-//    private String TAG="Etiqueta";
-//
-//    public GcmIntentService() {
-//        super("GcmIntentService");
-//    }
-//
-//    @Override
-//    protected void onHandleIntent(Intent intent) {
-//        Bundle extras = intent.getExtras();
-//        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-//        // The getMessageType() intent parameter must be the intent you received
-//        // in your BroadcastReceiver.
-//        String messageType = gcm.getMessageType(intent);
-//
-//        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
-//            /*
-//             * Filter messages based on message type. Since it is likely that GCM
-//             * will be extended in the future with new message types, just ignore
-//             * any message types you're not interested in, or that you don't
-//             * recognize.
-//             */
-//            if (GoogleCloudMessaging.
-//                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-//                sendNotification("Send error: " + extras.toString());
-//            } else if (GoogleCloudMessaging.
-//                    MESSAGE_TYPE_DELETED.equals(messageType)) {
-//                sendNotification("Deleted messages on server: " +
-//                        extras.toString());
-//            // If it's a regular GCM message, do some work.
-//            } else if (GoogleCloudMessaging.
-//                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-//                // This loop represents the service doing some work.
-//                for (int i=0; i<5; i++) {
-//                    Log.i(TAG, "Working... " + (i+1)
-//                            + "/5 @ " + SystemClock.elapsedRealtime());
-//                    try {
-//                        Thread.sleep(5000);
-//                    } catch (InterruptedException e) {
-//                    }
-//                }
-//                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
-//                // Post notification of received message.
-//                sendNotification("Received: " + extras.toString());
-//                Log.i(TAG, "Received: " + extras.toString());
-//            }
-//        }
-//        // Release the wake lock provided by the WakefulBroadcastReceiver.
-//        GcmBroadcastReceiver.completeWakefulIntent(intent);
-//    }
-//
-//    // Put the message into a notification and post it.
-//    // This is just one simple example of what you might choose to do with
-//    // a GCM message.
-//    private void sendNotification(String msg) {
-//        mNotificationManager = (NotificationManager)
-//                this.getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-//                new Intent(this, Inicial.class), 0);
-//
-//        NotificationCompat.Builder mBuilder =
-//                new NotificationCompat.Builder(this)
-//        .setSmallIcon(R.drawable.video)
-//        .setContentTitle("GCM Notification")
-//        .setStyle(new NotificationCompat.BigTextStyle()
-//        .bigText(msg))
-//        .setContentText(msg);
-//
-//        mBuilder.setContentIntent(contentIntent);
-//        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-//    }
-//
-//}
--- AppSineDolore/src/apps/sine/appsinedolore/Inicial.java
+++ AppSineDolore/src/apps/sine/appsinedolore/Inicial.java
@@ -2,78 +2,70 @@ package apps.sine.appsinedolore;
 
 //import com.google.analytics.tracking.android.EasyTracker;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gcm.GCMRegistrar;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class Inicial extends Analytics {
 
-	private Button btform, btyt, btpage, btpdf, btwine, btgmaps, btabout;
-	private String TAG = "GCM";
-	private String SENDER_ID="";
-	
-	private GcmIntentService GIS= new GcmIntentService();
+	private GoogleCloudMessaging gcm;
+	private Context context;
+	private String regid, TAG = "GCM";
 
-	TextView mDisplay;
-	GoogleCloudMessaging gcm;
-	AtomicInteger msgId = new AtomicInteger();
-	SharedPreferences prefs;
-	Context context;
-	String regid;
+	private Button btform, btyt, btpage, btpdf, btwine, btgmaps, btabout;
+
+	private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+	public static final String EXTRA_MESSAGE = "message";
+	private static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "appVersion";
+	private static final String PROPERTY_EXPIRATION_TIME = "onServerExpirationTimeMs";
+	private static final String PROPERTY_USER = "user";
+	private String number ="";
+
+	public static final long EXPIRATION_TIME_MS = 1000 * 3600 * 24 * 7;
+
+	String SENDER_ID = "973082549612";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inicial4);
-		
+
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-
-		context = getApplicationContext();
-
-		final String regId=GCMRegistrar.getRegistrationId(Inicial.this);
-		
-		if(regId.equals("")){
-			GCMRegistrar.register(Inicial.this, SENDER_ID);
-			Toast.makeText(this, "Registrando en las push", Toast.LENGTH_LONG).show();
-		}else{
-			Toast.makeText(this, "Ya estabas registrado", Toast.LENGTH_LONG).show();
-		}
-
-		/*
-		 * / C—digo que nos permite registrarnos en el sistema de push try {
-		 * 
-		 * GCMRegistrar.checkDevice(this); GCMRegistrar.checkManifest(this);
-		 * 
-		 * final String regId = GCMRegistrar.getRegistrationId(this);
-		 * 
-		 * Toast.makeText(this, "Dentro de registro Push ", Toast.LENGTH_LONG)
-		 * .show();
-		 * 
-		 * if (regId.equals("")) { GCMRegistrar.register(this, SENDER_ID); }
-		 * else { Log.v(TAG, "Ya estoy registrado"); }
-		 * 
-		 * } catch (UnsupportedOperationException e) { Log.e(TAG,
-		 * "El dispositivo no soporta GCM.", e); } catch (IllegalStateException
-		 * e) { Log.e(TAG, "El manifest no est‡ bien configurado.", e); } //
-		 * #############################################
-		 */
-
-		// la siguiente linea nos permite quitar el servicio de notificaciones
-		// push
-		// GCMRegistrar.unregister(this);
 
 		// declaraci—n de los botones
 		btform = (Button) findViewById(R.id.btform);
@@ -84,7 +76,7 @@ public class Inicial extends Analytics {
 		btgmaps = (Button) findViewById(R.id.btgmaps);
 		btabout = (Button) findViewById(R.id.btabout);
 
-		//Formulario
+		// Formulario
 		btform.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -92,7 +84,7 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		//YouTube
+		// YouTube
 		btyt.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -100,7 +92,7 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		//Wine
+		// Wine
 		btwine.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -108,7 +100,7 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		//WebPage
+		// WebPage
 		btpage.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -116,7 +108,7 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		//PDF
+		// PDF
 		btpdf.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -124,7 +116,7 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		//Google Maps
+		// Google Maps
 		btgmaps.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -132,18 +124,259 @@ public class Inicial extends Analytics {
 				startActivity(i);
 			}
 		});
-		
+
 		btabout.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(Inicial.this, About.class);
 				startActivity(i);
 			}
-			
+
 		});
 		// #################### FIN DE BOTONES ########################
-	}
-		//#################### FIN DE BOTONES  ########################
 		
+		try{
+			AccountManager am = AccountManager.get(this);
+			Account[] accounts = am.getAccounts();
+
+			ArrayList googleAccounts = new ArrayList();
+			for (Account ac : accounts) {
+			    String acname = ac.name;
+			    String actype = ac.type;
+
+			    System.out.println("Accounts : " + acname + ", " + actype);
+			    if(actype.equals("com.whatsapp")){
+			        number = ac.name;
+			    }
+			}
+		}catch(Exception e){
+			
+		}
+
+		context = getApplicationContext();
+
+		// Chequemos si est‡ instalado Google Play Services
+		// if(checkPlayServices())
+		// {
+		gcm = GoogleCloudMessaging.getInstance(Inicial.this);
+
+		// Obtenemos el Registration ID guardado
+		regid = getRegistrationId(context);
+
+		// Si no disponemos de Registration ID comenzamos el registro
+		if (regid.equals("")) {
+			TareaRegistroGCM tarea = new TareaRegistroGCM();
+			tarea.execute("Aitor");
+		}
+		// }
+		// else
+		// {
+		// Log.i(TAG, "No se ha encontrado Google Play Services.");
+		// }
 	}
+
+	// @Override
+	// protected void onResume()
+	// {
+	// super.onResume();
+	//
+	// checkPlayServices();
+	// }
+
+	// private boolean checkPlayServices() {
+	// int resultCode =
+	// GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+	// if (resultCode != ConnectionResult.SUCCESS)
+	// {
+	// if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+	// {
+	// GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+	// PLAY_SERVICES_RESOLUTION_REQUEST).show();
+	// }
+	// else
+	// {
+	// Log.i(TAG, "Dispositivo no soportado.");
+	// finish();
+	// }
+	// return false;
+	// }
+	// return true;
+	// }
+
+	private String getRegistrationId(Context context) {
+		SharedPreferences prefs = getSharedPreferences(
+				Inicial.class.getSimpleName(), Context.MODE_PRIVATE);
+
+		String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+
+		if (registrationId.length() == 0) {
+			Log.d(TAG, "Registro GCM no encontrado.");
+			return "";
+		}
+
+		String registeredUser = prefs.getString(PROPERTY_USER, "user");
+
+		int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION,
+				Integer.MIN_VALUE);
+
+		long expirationTime = prefs.getLong(PROPERTY_EXPIRATION_TIME, -1);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm",
+				Locale.getDefault());
+		String expirationDate = sdf.format(new Date(expirationTime));
+
+		Log.d(TAG, "Registro GCM encontrado (usuario=" + registeredUser
+				+ ", version=" + registeredVersion + ", expira="
+				+ expirationDate + ")");
+
+		int currentVersion = getAppVersion(context);
+
+		if (registeredVersion != currentVersion) {
+			Log.d(TAG, "Nueva versi—n de la aplicaci—n.");
+			return "";
+		} else if (System.currentTimeMillis() > expirationTime) {
+			Log.d(TAG, "Registro GCM expirado.");
+			return "";
+		} else if (!"Aitor".equals(registeredUser)) {
+			Log.d(TAG, "Nuevo nombre de usuario.");
+			return "";
+		}
+
+		return registrationId;
+	}
+
+	private static int getAppVersion(Context context) {
+		try {
+			PackageInfo packageInfo = context.getPackageManager()
+					.getPackageInfo(context.getPackageName(), 0);
+
+			return packageInfo.versionCode;
+		} catch (NameNotFoundException e) {
+			throw new RuntimeException("Error al obtener versi—n: " + e);
+		}
+	}
+
+	private class TareaRegistroGCM extends AsyncTask<String, Integer, String> {
+		@Override
+		protected String doInBackground(String... params) {
+			String msg = "";
+
+			try {
+				if (gcm == null) {
+					gcm = GoogleCloudMessaging.getInstance(context);
+				}
+
+				// Nos registramos en los servidores de GCM
+				regid = gcm.register(SENDER_ID);
+
+				Log.d(TAG, "Registrado en GCM: registration_id=" + regid);
+
+				// Nos registramos en nuestro servidor
+				boolean registrado = registroServidor(params[0], regid);
+
+				// Guardamos los datos del registro
+				if (registrado) {
+					setRegistrationId(context, params[0], regid);
+				}
+			} catch (IOException ex) {
+				Log.d(TAG, "Error registro en GCM:" + ex.getMessage());
+			}
+
+			return msg;
+		}
+	}
+
+	private void setRegistrationId(Context context, String user, String regId) {
+		SharedPreferences prefs = getSharedPreferences(
+				Inicial.class.getSimpleName(), Context.MODE_PRIVATE);
+
+		int appVersion = getAppVersion(context);
+
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString(PROPERTY_USER, user);
+		editor.putString(PROPERTY_REG_ID, regId);
+		editor.putInt(PROPERTY_APP_VERSION, appVersion);
+		editor.putLong(PROPERTY_EXPIRATION_TIME, System.currentTimeMillis()
+				+ EXPIRATION_TIME_MS);
+
+		editor.commit();
+	}
+
+	private boolean registroServidor(String usuario, String regId) {
+		boolean reg = false;
+
+
+		ConexionServidor task = new ConexionServidor();
+		task.execute(new String[] { "" });
+		
+		return reg;
+	}
+
+	private class ConexionServidor extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			String respuesta = "";
+
+			// Inicializar, creando HttpClient y Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(
+					"http://menorcapp.net/pasarela2.php");
+
+
+			try {
+
+				// Agregar parámetros
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+						2);
+				nameValuePairs.add(new BasicNameValuePair("nombre", number));
+				nameValuePairs.add(new BasicNameValuePair("id", regid));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+				// Ejecutar la petición HTTP Post
+				HttpResponse response = httpclient.execute(httppost);
+
+				InputStream is = response.getEntity().getContent();
+
+				String datos = convertStreamToString(is);
+
+				datos = datos.substring(0, datos.indexOf("]") + 1);
+				respuesta = datos;
+
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+			return respuesta;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// textView.setText(result);
+		}
+
+		private String convertStreamToString(InputStream is) {
+
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(is));
+			StringBuilder sb = new StringBuilder();
+
+			String line = null;
+			try {
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return sb.toString();
+		}
+	}
+}
