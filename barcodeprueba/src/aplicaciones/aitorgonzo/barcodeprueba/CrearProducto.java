976d1e04e0c86982cafed802c31574665d37eb02
Gonzo345 <gonzo345@gmail.com>
2014-05-19 13:55:17 +0200

    Imagen en producto
    Posibilidad de añadir imagen a producto (galería o cámara). No se envía
    la foto junto al resto de datos.

--- barcodeprueba/src/aplicaciones/aitorgonzo/barcodeprueba/CrearProducto.java
+++ barcodeprueba/src/aplicaciones/aitorgonzo/barcodeprueba/CrearProducto.java
@@ -1,6 +1,9 @@
 package aplicaciones.aitorgonzo.barcodeprueba;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,17 +21,27 @@ import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.example.barcodeprueba.R;
 
 public class CrearProducto extends Activity{
 	
+	private RadioButton rbfull, rbgallery;
 	public String URL = "http://www.menorcapp.net/pasarelaInsertScann.php",
 			URLExiste = "http://www.menorcapp.net/existe.php?id=",
 			str_id = "no especificado", str_nombre = "no especificado",
@@ -36,10 +49,15 @@ public class CrearProducto extends Activity{
 			str_descripcion = "no especificado",
 			str_supermercado = "no especificado",
 			str_marca = "no especificado";
-	private String id="";
+	private String id="",name="";
+	
+	//Constantes para identificar la acci—n
+	private static int TAKE_PICTURE = 1;
+	private static int SELECT_PICTURE = 2;
 	
 	private TextView txname, txcost, txid, txmarca;
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.crearproducto);
 		
@@ -48,9 +66,18 @@ public class CrearProducto extends Activity{
 		txid = (TextView) findViewById(R.id.txid);
 		txmarca = (TextView) findViewById(R.id.txmarca);
 		
-		Button btenviar = (Button) findViewById(R.id.btenviar);
+		name= Environment.getExternalStorageDirectory() + "/test.jpg";
+		
+		Button btenviar = (Button) findViewById(R.id.btenviar);	//Confirmamos env’o de datos
+		Button btfoto = (Button) findViewById(R.id.btfoto);	//Bot—n para foto
+		
+		
+		rbfull = (RadioButton) findViewById(R.id.rbcamara);
+		rbgallery = (RadioButton) findViewById(R.id.rbgaleria);
+		
+		
 		btenviar.setOnClickListener(new OnClickListener() {
-
+			//Al hacer click en bot—n enviar env’a informai—n a servidor
 			@Override
 			public void onClick(View arg0) {
 			
@@ -61,6 +88,32 @@ public class CrearProducto extends Activity{
 				
 				EnviarDatosInsert(URL);
 				
+			}
+		});
+		
+		btfoto.setOnClickListener(new OnClickListener() {
+			//Al hacer click en boton foto hace foto
+			@Override
+			public void onClick(View arg0) {
+			
+				int code = TAKE_PICTURE;	
+				Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				
+				//Usamos los radiobuttons
+				if (rbfull.isChecked()) {
+					
+					//Tiramos de c‡mara
+					Uri output = Uri.fromFile(new File(name));
+					intent.putExtra(MediaStore.EXTRA_OUTPUT, output);
+				} 
+				else if (rbgallery.isChecked()){
+					
+					//Tiramos de galer’a
+					intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+					code = SELECT_PICTURE;
+				}
+				
+				startActivityForResult(intent, code);
 			}
 		});
 		
@@ -78,6 +131,55 @@ public class CrearProducto extends Activity{
 		TextView barcode = (TextView) findViewById(R.id.txid);
 		barcode.setText(id);
 	}
+	
+	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	 //Comprobamos si la imagen viene de la c‡mara (TAKE_PICTURE) o de la galer’a (SELECT_PICTURE)
+    	if (requestCode == TAKE_PICTURE) {
+    		 //Si se reciben datos en el intent tenemos una vista previa (thumbnail)
+    		
+    		if (data != null) {
+    			
+    			//En el caso de una vista previa, obtenemos el extra data del intent y lo mostramos en el ImageView
+    			if (data.hasExtra("data")) { 
+    				ImageView iv = (ImageView)findViewById(R.id.imagen);
+    				iv.setImageBitmap((Bitmap) data.getParcelableExtra("data"));
+    			}
+    		//Sino, es una imagen completa  			
+    		} else {
+    			
+    			//A partir del nombre del archivo ya definido lo buscamos y creamos el bitmap para el ImageView
+    			ImageView iv = (ImageView)findViewById(R.id.imagen);
+    			
+    			//Asignamos imagen a ImageView
+    			iv.setImageBitmap(BitmapFactory.decodeFile(name));
+    			
+    			//Para guardar la imagen en la galer’a, utilizamos una conexi—n a un MediaScanner
+    			new MediaScannerConnectionClient() {
+    				private MediaScannerConnection msc = null; {
+    					msc = new MediaScannerConnection(getApplicationContext(), this); msc.connect();
+    				}
+    				public void onMediaScannerConnected() { 
+    					msc.scanFile(name, null);
+    				}
+    				public void onScanCompleted(String path, Uri uri) { 
+    					msc.disconnect();
+    				} 
+    			};				
+    		}
+    	
+    		//Recibimos el URI de la imagen y construimos un Bitmap a partir de un stream de Bytes
+    	} else if (requestCode == SELECT_PICTURE){
+    		Uri selectedImage = data.getData();
+    		InputStream is;
+    		try {
+    			is = getContentResolver().openInputStream(selectedImage);
+    	    	BufferedInputStream bis = new BufferedInputStream(is);
+    	    	Bitmap bitmap = BitmapFactory.decodeStream(bis);            
+    	    	ImageView iv = (ImageView)findViewById(R.id.imagen);
+    	    	iv.setImageBitmap(bitmap);						
+    		} catch (FileNotFoundException e) {}
+    	}
+    }
 	
 	public void EnviarDatosInsert(String URL) {
 
